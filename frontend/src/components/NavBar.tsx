49a97249feeec62597f73d30e51df2e3c1f55501
Stephen-PP <pierrepaulstephen@gmail.com>
2024-04-03 12:13:27 -0400

    added hero section + refactored directories

---
+++ frontend/src/components/NavBar.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { Icon } from '@iconify/react';
+import Image from "next/image";
+
+export default function NavBar() {
+    return (
+        <div className="flex justify-center">
+            <div className="mt-5 flex justify-between w-full max-w-screen-2xl">
+                <div className="flex-initial self-center">
+                    <Image src="/logo-light.svg" alt="Logo" width={100} height={100} />
+                </div>
+                <div className="flex items-right justify-between">
+                    <div className="flex px-6 py-3">
+                    <p className="font-inter font-semibold">GitHub</p>
+                    </div>
+                    <div className="flex border border-black rounded px-6 py-3 ml-6">
+                        <Icon className="h-5 w-5 self-center " style={{color: "black"}} icon="ph:moon-fill"/>
+                    </div>
+                    <div className="flex border border-black rounded px-6 py-3 ml-6">
+                        <Icon className="h-5 w-5 self-center mr-2" icon="solar:settings-linear"/>
+                        <p className="font-inter font-semibold">Global Settings</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ frontend/src/components/hero/Hero.tsx
@@ -0,0 +1,14 @@
+export default function HeroSection() {
+    return (
+        <div className="flex justify-center">
+            <div className="mt-36 w-full max-w-screen-2xl text-center">
+                <div>
+                    <p className="font-inter font-semibold text-6xl">Compress your files with ease</p>
+                </div>
+                <div className="mt-4">
+                    <p className="font-inter text-lg">Compress <span className="font-semibold">PDF, JPG, PNG, MP3</span> files and more</p>
+                </div>
+            </div>
+        </div>
+    )
+}
